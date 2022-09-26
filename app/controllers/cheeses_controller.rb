69f2b1f942eebb8f0b4c61a3ab37024daf15d023
Mutoi01 <pngando9@gmail.com>
2022-09-26 15:14:32 +0300

    commit tree 9adb50187094abb21364e74313cabce02fb656f0

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,17 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name, :price, :is_best_seller], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
     render json: cheese
+  end
+
+  def summary
+    "#{name} $#{price}"
   end
 
 end
